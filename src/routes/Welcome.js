2e56075e9a0757fd560556627183eaa24b8aef50
CecileAnna <6263325@RTS-365.dk>
2022-10-04 16:23:08 +0200

    Work on welcome

--- src/routes/Welcome.js
+++ src/routes/Welcome.js
@@ -1,7 +1,20 @@
+import Btn from "../components/Btn";
+import { Link } from "react-router-dom";
+
 const Welcome = () => {
   return (
     <>
-      <h2>This is a Welcome page!</h2>
+      <main className="welcome--main-wrapper welcome--background-img">
+        <div className="welcome--logo-text">
+          <h1 className="welcome--title">
+            Landrup
+            <span className="welcome--title-span">Dans</span>
+          </h1>
+        </div>
+        <Link to={`/activities`} style={{ textDecoration: "none" }}>
+          <Btn text="Kom i gang" styles="welcome--btn" />
+        </Link>
+      </main>
     </>
   );
 };
