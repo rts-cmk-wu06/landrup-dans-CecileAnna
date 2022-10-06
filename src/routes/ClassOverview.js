8ea7994d8fbda66e041b68012e59d74f86f411c1
CecileAnna <6263325@RTS-365.dk>
2022-10-06 13:53:16 +0200

    work on class overview page

--- src/routes/ClassOverview.js
+++ src/routes/ClassOverview.js
@@ -1,9 +1,63 @@
 import FooterMenu from "../components/FooterMenu";
+import Heading2 from "../components/subcomponents/texts/Heading2";
+import Paragraf from "../components/subcomponents/texts/Paragraf";
+import WrapperCenterContent from "../components/WrapperCenterContent";
+
+import { useParams } from "react-router-dom";
+import axios from "../apis/axios";
+import { useState, useEffect } from "react";
 
 const ClassOverview = () => {
+  let { id } = useParams();
+
+  const ACTIVITY_URL = `api/v1/activities/${id}`;
+
+  const [activityData, setActivityData] = useState();
+
+  useEffect(() => {
+    fetchActivity();
+    // eslint-disable-next-line
+  }, [id, activityData?.length]);
+
+  const fetchActivity = async () => {
+    try {
+      const response = await axios.get(ACTIVITY_URL);
+
+      let activity = response?.data;
+
+      setActivityData(activity);
+
+      console.log(activityData && activityData);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
     <>
-      <h2>This is a page for ClassOverview</h2>
+      <WrapperCenterContent>
+
+              {activityData?.name && (
+              
+                <>
+                  <Heading2 text={activityData.name} />
+
+                  <section className="class-overview--participants-wrapper">
+                    {activityData.users.map((user) => (
+                      <Paragraf
+                        text={`${user.firstname} ${user.lastname}`}
+                        key={user.id}
+                        styles="white-color-important"
+                      />
+                    ))}
+                  </section>
+                </>
+
+                )}
+              
+          
+      </WrapperCenterContent>
+  
       <FooterMenu />
     </>
   );
