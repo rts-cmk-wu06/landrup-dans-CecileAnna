1d2a1c08eee54ecb820e1d31c7640d5f03554c87
CecileAnna <6263325@RTS-365.dk>
2022-10-07 10:59:50 +0200

    first attempts at setting up yup

--- src/components/Login.js
+++ src/components/Login.js
@@ -4,6 +4,28 @@ import { useNavigate } from "react-router-dom";
 import { useRef, useState, useEffect } from "react";
 import { useAuth } from "../context/AuthProvider";
 import axios from "../apis/axios";
+import * as yup from 'yup';
+
+let schema = yup.object().shape({
+  username: yup.string().required(),
+  password: yup
+    .string()
+    .required("Please provide a valid password")
+    .oneOf([yup.ref("password"), null])
+    .min(8, "Password needs to be a minimum of 8 characters"),
+  createdOn: yup.date().default(function() {
+    return new Date();
+  }),
+});
+
+// // check validity
+// schema
+//   .isValid({
+
+//   })
+//   .then(function (valid) {
+//     valid; // => true
+//   });
 
 const LOGIN_URL = "auth/token";
 
